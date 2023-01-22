2535a4c014fd002753b9c16366cb815f8b575454
Pankaj <myemail@email.com>
2023-01-22 12:40:42 +0530

    this is commit

--- src/main/java/com/xe/demo/resource/TaskResource.java
+++ src/main/java/com/xe/demo/resource/TaskResource.java
@@ -2,6 +2,7 @@ package com.xe.demo.resource;
 
 import com.xe.demo.resource.response.TaskResponse;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/task")
 public class TaskResource {
 
-   /* public ResponseEntity<TaskResponse> getTasks(@RequestBody(value="id", required = false) int id){
-
-    }*/
+  @GetMapping("/find")
+    public String findMyName(){
+      return "Supriya";
+  }
+    @GetMapping("/TEST")
+    public String getName(String name){
+        String task = "Pankaj";
+        return task;
+    }
 }
 
 
